544ca2830ac08e97dbfe4843d01fedae78414e27
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2023-09-11 16:22:00 +0530

    new pr

--- cal.py
+++ cal.py
@@ -1,6 +1,9 @@
 def add():
     result = 2 + 3
     print("2 + 3 =", result)
-
+def subs():
+    print("3-2",3-2)
 # Call the add function to perform the addition and print the result
+print("ok")
 add()
+subs()
