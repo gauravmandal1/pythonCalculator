3e94df8c27249b01fac89767efee0f13c6f78475
gauravmandal1 <64638825+gauravmandal1@users.noreply.github.com>
2023-09-11 15:49:27 +0530

    subs complete

--- cal.py
+++ cal.py
@@ -1,6 +1,8 @@
 def add():
     result = 2 + 3
     print("2 + 3 =", result)
-
+def subs():
+    print("3-2",3-2)
 # Call the add function to perform the addition and print the result
 add()
+subs()
